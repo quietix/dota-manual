bdaa944723d78d6e9affe1e036020afc6ba4932b
Ivan <i.o.zashchyk@gmail.com>
2024-02-18 22:30:00 +0200

    feat: can send response as photo

--- src/dota_manual_app/urls.py
+++ src/dota_manual_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+
+import dota_manual_app.views
 from .views import HomePageView
 from django.conf.urls.static import static  # new
 from server import settings
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("juggernaut", dota_manual_app.views.send_file),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/dota_manual_app/views.py
+++ src/dota_manual_app/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
 from .models import Build
+from django.http import FileResponse
+
+def send_file(response):
+    img = open('media/images/juggernaut.png', 'rb')
+    response = FileResponse(img)
+    return response
 
 
 class HomePageView(ListView):
     model = Build
     template_name = "home.html"
+
+
